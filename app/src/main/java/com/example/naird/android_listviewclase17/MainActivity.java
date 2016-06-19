dbfb24676238812484646f8cafce06c19bb3327e
addyyarci <adi.fuente3@gmail.com>
2016-06-19 12:40:11 -0500

    commit 20160619_1239pm

--- app/src/main/java/com/example/naird/android_listviewclase17/MainActivity.java
+++ app/src/main/java/com/example/naird/android_listviewclase17/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.naird.android_listviewclase17;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView ListView;
+    String[] valores = new String[] {"Holanda", "España", "India", "Mexico", "China", "Uruguay", "Argentina", "Brasil"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView = (ListView) findViewById(R.id.Lista);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,valores);
+        ListView.setAdapter(adapter);
+
+        ListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "Position " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
